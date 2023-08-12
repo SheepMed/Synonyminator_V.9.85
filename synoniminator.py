6b844c8d3db7963c86af6b6da75f62f75a9e1139
Sheep <141952433+SheepMed@users.noreply.github.com>
2023-08-12 16:19:01 +0100

    Add files via upload

--- synoniminator.py
+++ synoniminator.py
@@ -3,39 +3,47 @@ import re
 import tkinter as tk
 from tkinter import messagebox, scrolledtext
 
-def fetch_synonyms(word):
+def fetch_words(word, is_synonym=True):
     url = f"https://www.thesaurus.com/browse/{word}"
     response = requests.get(url)    
-    synonyms = []
+    words = []
     if response.status_code == 200:
         page_content = response.text
-        synonyms_text = re.search(r'"synonyms":\[(.*?)\]', page_content).group(1)
-        synonyms = [syn.strip('"\t ') for syn in re.findall(r'"term":"(.*?)"', synonyms_text) if not any(flag in syn for flag in ["target", "similarity", "isinformal", "isvulgar"])]    
-    return synonyms
+        key = "synonyms" if is_synonym else "antonyms"
+        words_text = re.search(rf'"{key}":\[(.*?)\]', page_content).group(1)
+        words = [w.strip('"\t ') for w in re.findall(r'"term":"(.*?)"', words_text) if not any(flag in w for flag in ["target", "similarity", "isinformal", "isvulgar"])]
+    
+    return words
 
-def display_synonyms(synonyms, word):
-    if synonyms:
-        synonyms_output.config(state=tk.NORMAL)
-        synonyms_output.delete("1.0", tk.END)
-        synonyms_output.insert(tk.END, f"Synonyms for '{word}':\n")
-        for idx, synonym in enumerate(synonyms, start=1):
-            synonyms_output.insert(tk.END, f"{idx}. {synonym}\n")
-        synonyms_output.config(state=tk.DISABLED)
+def display_words(words, word, is_synonym=True):
+    words_output.config(state=tk.NORMAL)
+    words_output.delete("1.0", tk.END)
+    
+    if words:
+        key = "Synonyms" if is_synonym else "Antonyms"
+        words_output.insert(tk.END, f"{key} for '{word}':\n")
+        for idx, w in enumerate(words, start=1):
+            words_output.insert(tk.END, f"{idx}. {w}\n")
     else:
-        messagebox.showinfo("No Synonyms", f"No synonyms found for '{word}'.")
+        key = "Synonyms" if is_synonym else "Antonyms"
+        words_output.insert(tk.END, f"No {key.lower()} found for '{word}'.\n")
+    
+    words_output.config(state=tk.DISABLED)
 
-def find_synonyms():
+def find_words(is_synonym=True):
     word = entry_word.get()
-    synonyms = fetch_synonyms(word)
-    display_synonyms(synonyms, word)
+    words = fetch_words(word, is_synonym)
+    display_words(words, word, is_synonym)
 app = tk.Tk()
-app.title("Synonym Finder")
+app.title("Synonym and Antonym Finder")
 label_word = tk.Label(app, text="Enter a word:")
 entry_word = tk.Entry(app)
-button_find = tk.Button(app, text="Find Synonyms", command=find_synonyms)
-synonyms_output = scrolledtext.ScrolledText(app, state=tk.DISABLED, wrap=tk.WORD)
+button_find_synonyms = tk.Button(app, text="Find Synonyms", command=lambda: find_words(is_synonym=True))
+button_find_antonyms = tk.Button(app, text="Find Antonyms", command=lambda: find_words(is_synonym=False))
+words_output = scrolledtext.ScrolledText(app, state=tk.DISABLED, wrap=tk.WORD)
 label_word.pack(pady=5)
 entry_word.pack(pady=5)
-button_find.pack(pady=5)
-synonyms_output.pack(padx=5, pady=5, fill=tk.BOTH, expand=True)
+button_find_synonyms.pack(pady=5)
+button_find_antonyms.pack(pady=5)
+words_output.pack(padx=5, pady=5, fill=tk.BOTH, expand=True)
 app.mainloop()
